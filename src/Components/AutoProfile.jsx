752ea29310dfeacf3cc54dc5ba17621144e7d9a0
Govind1732 <146473888+Govind1732@users.noreply.github.com>
2024-04-02 22:19:39 +0530

    Update AutoProfile.jsx

--- src/Components/AutoProfile.jsx
+++ src/Components/AutoProfile.jsx
@@ -1,63 +1,125 @@
-import React from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import React, { useState, CSSProperties } from "react";
+import { Container, Row, Col, Form, Button, Breadcrumb, Modal } from "react-bootstrap";
+import axios from "axios";
+import { GridLoader } from "react-spinners";
+import { css } from '@emotion/react'
+// import URLSearchParams from 'url-search-params'
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
 
 const AutoProfile = () => {
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const fileInput = e.target.elements.formFile;
+  const [file, setFile] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    setShow(false);
+  }
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+
+    if (selectedFile && selectedFile.type === 'text/csv') {
+      setFile(selectedFile)
+    }
+    else {
+      alert('Please select a csv file')
+    }
+  }
+  const handleUpload = () => {
 
     // Check if files were selected
-    if (!fileInput.files || fileInput.files.length === 0) {
-        alert('Please select a file');
-        return;
+    if (!file) {
+      alert('Please select a file');
+      return;
     }
+    const formData = new FormData()
+    setLoading(true)
+    formData.append('file', file)
 
-    const file = fileInput.files[0];
+    console.log(loading)
 
-    // Check if the selected file is a CSV file
-    if (!file.name.toLowerCase().endsWith('.csv')) {
-        alert('Please select a CSV file');
-        return;
-    }
-};
+    // const formData = new URLSearchParams();
+    // formData.append('file', file);
+
+
+    axios.post('http://tdcldizcva002.ebiz.verizon.com:8000/mle/MLESelfServe/', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+      .then(response => {
+        console.log(response.data)
+        setLoading(false)
+      })
+      .catch(error => {
+        console.error('Error uploading file: ', error);
+        setLoading(false)
+      });
+  };
+
+
+
+  const resetHandler = () => {
+
+  }
+
+
 
   return (
-    <Container fluid className="">
-      <Row className="justify-content-center align-items-center my-5">
-        <Col xl={5}>
-          <div className="px-5 py-3 rounded shadow-lg">
-            <h2 className="mb-4 text-center">Auto Profile Form</h2>
+    <Container fluid className={loading?'blur':''}>
+      <Container fluid className="my-2 pt-3">
+        <Breadcrumb>
+          <Breadcrumb.Item active>Auto Profile</Breadcrumb.Item>
+          <Breadcrumb.Item active><span className='fw-bold'>Auto Profile Submit Request</span></Breadcrumb.Item>
 
-            <Form onSubmit={handleSubmit}>
+        </Breadcrumb>
+      </Container>
+
+      <Container fluid className="mx-8 px-8 mb-2">
+        <Row className="justify-content-center align-items-center my-5">
+          <Col xl={5}>
+            <div className="px-5 py-3">
+              <h2 className="mb-4 text-center">Auto Profile Form</h2>
+
+
               <h6>dqaas_auto_prfl_mtd_&lt;proj_name&gt;.csv</h6>
 
               <Form.Group controlId="formFile" className="my-3">
-                {/* <Form.Label>Only csv file format is allowed</Form.Label> */}
-                <Form.Control type="file" accept=".csv" />
+
+                <Form.Control type="file" accept=".csv" onChange={handleFileChange} />
               </Form.Group>
 
               <a href="/assets/dqaas_auto_prfl_mtd_tier1.csv" download className="text-decoration-none text-danger">
                 Download sample Template(CSV)
               </a>
               <div className="d-flex justify-content-center my-3">
-              <Button variant="dark" type="submit">
+                <Button variant="dark" onClick={resetHandler}>
+                  Reset
+                </Button>
+                <Button variant="dark" type="submit" onClick={handleUpload}>
                   Submit
                 </Button>
               </div>
-            </Form>
-            
-          </div>
-          {/* <div className="d-flex justify-content-around my-3">
-                <Button variant="dark" type="submit">
-                  view mtd
-                </Button>
-                <Button variant="dark" type="submit">
-                  Edit mtd
-                </Button>
-              </div> */}
-        </Col>
-      </Row>
+
+
+            </div>
+
+          </Col>
+        </Row>
+      </Container>
+
+      {loading &&
+        <Container className="loading-overlay">
+          <GridLoader color="#ff0000" loading={loading} cssOverride={override} size={50} aria-label="Loading Spinner" data-testid='loader' />
+        </Container>
+      }
     </Container>
   );
 };
